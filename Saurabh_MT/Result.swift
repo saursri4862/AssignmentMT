6a019ea25ec094540918d4bac8ca5269c4961fbf
saur4862 <saurabhsri2901@gmail.com>
2018-07-29 14:49:02 +0530

    done this assignment

---
+++ Saurabh_MT/Result.swift
@@ -0,0 +1,9 @@
+//
+//  Result.swift
+//  Saurabh_MT
+//
+//  Created by saurabh srivastava on 29/07/18.
+//  Copyright © 2018 saurabh. All rights reserved.
+//
+
+import Foundation
---
+++ Saurabh_MT/WikiTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  WikiTableCell.swift
+//  Saurabh_MT
+//
+//  Created by saurabh srivastava on 29/07/18.
+//  Copyright © 2018 saurabh. All rights reserved.
+//
+
+import UIKit
+
+class WikiTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
